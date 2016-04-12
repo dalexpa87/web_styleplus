59349a4051ccbdc1a99da39d706f6b93c8627105
dalexpa87 <dalexpa@misena.edu.co>
2016-04-11 23:24:34 -0500

    Pagina Web y Web Promo
    Se construyen los archivos de la web respectiva, pendiente de
    modificaciones

---
+++ web/index_promo.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link type="text/css" rel="stylesheet" href="materialize/css/materialize.css"  media="screen,projection"/>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<title>Web_promo</title>
+</head>
+<header>
+	<div class="row">
+		<div class ="col s12 black">
+			<div class="col s4">
+			<img id="logo_sp" src="img/logo.png">
+			</div>
+			<div class="col s8">
+				<h4 class="white-text center">Una aplicación con estilo, para personas como tú </h4>
+			</div>
+	</div>
+</header>		
+<section>
+	<div class="row">
+		<div class="col s12 white" class="center">
+					<div class="col s4">
+						<h1 class="black-text center">Diseño Responsivo</h1>
+						<h5 class="black-text center">Puedes usar la aplicación en el dispositivo que desees, logrando una navegación constante en nuestro sitio y estar enterando de los contenidos publicados que serán de su interes</h5>
+						<a id="boton_ir" class="waves-effect cyan darken-3 btn-large "></i>IR A LA APLICACIÓN</a>
+					</div>
+			<div class="col s4">
+				<img id="pc1" src="img/webpc.png">
+			</div>
+			<div class="col s3">
+				<img id="movil1" src="img/movil1web.png">
+			</div>
+		</div>
+	</div>	
+</section>
+		
+		<section>
+			<div class="row">
+				<div class="col s12 grey lighten-2">
+					<div class="col s6">
+					<img id="movil1" class="materialboxed" src="img/movil2web.png">
+					</div>
+					<div class="col s6">
+						<h1 class="black-text center">Más de lo que te gusta</h1>
+						<h5 class="black-text center">Encuentra lo que más te gusta en los mejores lugares de la ciudad, esta aplicación tiene información precisa y actualizada de los descuentos, ofertas, nuevos productos; y todo aquello de lo que te gusta desde la comodidad de su casa o en su móvil</h5>
+					</div>
+				</div>	
+			</div>
+		
+			</div>
+			<div class="row">
+				<div class="col s12 grey lighten-3">
+					<div class="col s6">
+						<h1 class="black-text center">Información Personalizada</h1>
+						<h5 class="black-text center">La plena satisfacción del cliente nos distingue, lleva siempre contigo el registro de los usos que has dado a los servicios obtenidos a través de la aplicación</h5>
+					</div>
+					<div class="col s6">
+					<img id="movil1" class="materialboxed" src="img/movil3web.png">
+					</div>
+					
+				</div>
+			</div>
+		</section>
+
+<body>
+
+	<script type="text/javascript" src="jquery-1.12.1.min.js"></script>
+ 	<script type="text/javascript" src="materialize\js\materialize.min.js"></script>
+ 	<script> 
+	 	$(document).ready(function(){
+	    $('.materialboxed').materialbox();
+	  });
+  </script>
+
+</body>
+</html>
